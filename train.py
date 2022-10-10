8704a0eb714b3aeb9ff652842681a141c9793bf3
Vatsal <vatsalmpatel@gmail.com>
2022-10-10 18:25:15 -0400

    training loop in train file

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from utils import tokenize, stem, bag_of_words
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import Net
 
 with open('data.json','r') as f:
     intents = json.load(f)
@@ -51,6 +52,49 @@ class ChatDataset(Dataset):
         return self.n_samples
     
 batch_size = 8
+input_size = len(x_train[0])
+hidden_size = 8
+output_size = len(tags)
+learning_rate = 0.001
+epochs = 1000
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net(input_size,hidden_size,output_size).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+
+for epoch in range(epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
+
+print(f'final loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
