2becfa48cf5f311fc3bc195273568f2cd9f5ad56
Vatsal <vatsalmpatel@gmail.com>
2022-10-10 17:54:49 -0400

    modified train file and updated gitignore

---
+++ train.py
@@ -0,0 +1,36 @@
+import json
+import numpy as np
+from utils import tokenize, stem, bag_of_words
+
+
+with open('data.json','r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w,tag))
+
+ignore_words = ['?','!','.',',']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+x_train = []
+y_train = []
+
+for (pattern,tag) in xy:
+    bag = bag_of_words(pattern,all_words)
+    x_train.append(bag)
+    label = tags.index(tag)
+    y_train.append(label)
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
