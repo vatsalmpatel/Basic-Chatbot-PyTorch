36e3bda050ec1664f739e4bee2ef46f17888c6ca
Vatsal <vatsalmpatel@gmail.com>
2022-10-10 18:06:20 -0400

    created dataset for the model training

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import json
 import numpy as np
 from utils import tokenize, stem, bag_of_words
-
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('data.json','r') as f:
     intents = json.load(f)
@@ -32,5 +34,23 @@ for (pattern,tag) in xy:
     x_train.append(bag)
     label = tags.index(tag)
     y_train.append(label)
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+    
+    def __getitem__(self,idx):
+        return self.x_data[idx], self.y_data[idx]
+    
+    def __len__(self):
+        return self.n_samples
+    
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True)
